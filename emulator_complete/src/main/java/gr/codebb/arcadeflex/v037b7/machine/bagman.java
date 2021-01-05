ae0389fc649bbde7cf69280ad1b6b42e9b01d3ad
georgemoralis <giorgosmrls@gmail.com>
2021-01-05 07:39:43 +0200

    Added working 1942,1943,bagman,bankp

---
+++ emulator_complete/src/main/java/gr/codebb/arcadeflex/v037b7/machine/bagman.java
@@ -0,0 +1,256 @@
+/*
+ * ported to v0.37b7
+ * using automatic conversion tool v0.01
+ */
+package gr.codebb.arcadeflex.v037b7.machine;
+
+import static gr.codebb.arcadeflex.v037b7.common.fucPtr.*;
+
+public class bagman {
+
+  /*Creation date: 98-02-18 */
+  /*  A few words of comment:
+   **
+   **   What's inside of this file is a PAL16r6 emulator. Maybe someday we will
+   **need to use it for some other game too. We will need to make it more exact
+   **then (some of the functionality of this chip IS NOT implemented). However I
+   **have bought a book about PALs and I'm able to improve it. Just LMK.
+   **	Jarek Burczynski
+   **	bujar@mame.net
+   */
+  /*table holds outputs of all ANDs (after AND map)*/
+  static /*unsigned*/ char[] u8_andmap = new char[64];
+
+  /*table holds inputs (ie. not x, x, not q, q) to the AND map*/
+  static /*unsigned*/ char[] u8_columnvalue = new char[32];
+
+  /*8 output pins (actually 6 output and 2 input/output)*/
+  static /*unsigned*/ char[] u8_outvalue = new char[8];
+
+  /*		64 rows x 32 columns
+   **  1 - fuse blown: disconnected from input (equal to 1)
+   **  0 - fuse not blown: connected to input (ie. not x, x, not q, q accordingly)
+   */
+  static /*unsigned char*/ int fusemap[] = {
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1,
+    1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1,
+    1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1,
+    1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1,
+    1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1,
+    1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  };
+
+  static void update_pal() {
+    char rowoffs;
+    /*unsigned*/
+    char u8_row, u8_column, u8_val;
+
+    /*calculate all rows ANDs*/
+    for (u8_row = 0; u8_row < 64; u8_row++) {
+      rowoffs = (char) (u8_row * 32);
+      u8_val = 1;
+      /*prepare for AND */
+      for (u8_column = 0; u8_column < 32; u8_column++) {
+        if (fusemap[rowoffs + u8_column] == 0) {
+          u8_val &= u8_columnvalue[u8_column];
+        }
+      }
+      u8_andmap[u8_row] = u8_val;
+    }
+
+    /* I/O pin #19 */
+    u8_val = 0;
+    /*prepare for OR*/
+    for (u8_row = 1; u8_row < 8; u8_row++) {
+      u8_val |= u8_andmap[u8_row];
+    }
+    if (u8_andmap[0] == 1) {
+      u8_columnvalue[2] = (char) ((1 - u8_val) & 0xFF);
+      u8_columnvalue[3] = (char) ((u8_val) & 0xFF);
+      u8_outvalue[0] = (char) ((1 - u8_val) & 0xFF);
+    } else {
+      /*pin is in INPUT configuration so it doesn't create output...*/
+      u8_columnvalue[2] = 0;
+      u8_columnvalue[3] = 1;
+    }
+
+    /* O pin #18 (D1) */
+    u8_val = 0;
+    /*prepare for OR*/
+    for (u8_row = 8; u8_row < 16; u8_row++) {
+      u8_val |= u8_andmap[u8_row];
+    }
+    u8_columnvalue[6] = (char) ((1 - u8_val) & 0xFF);
+    u8_columnvalue[7] = (char) ((u8_val) & 0xFF);
+    u8_outvalue[1] = (char) ((1 - u8_val) & 0xFF);
+
+    /* O pin #17 (D2) */
+    u8_val = 0;
+    /*prepare for OR*/
+    for (u8_row = 16; u8_row < 24; u8_row++) {
+      u8_val |= u8_andmap[u8_row];
+    }
+    u8_columnvalue[10] = (char) ((1 - u8_val) & 0xFF);
+    u8_columnvalue[11] = (char) ((u8_val) & 0xFF);
+    u8_outvalue[2] = (char) ((1 - u8_val) & 0xFF);
+
+    /* O pin #16 (D3) */
+    u8_val = 0;
+    /*prepare for OR*/
+    for (u8_row = 24; u8_row < 32; u8_row++) {
+      u8_val |= u8_andmap[u8_row];
+    }
+    u8_columnvalue[14] = (char) ((1 - u8_val) & 0xFF);
+    u8_columnvalue[15] = (char) ((u8_val) & 0xFF);
+    u8_outvalue[3] = (char) ((1 - u8_val) & 0xFF);
+
+    /* O pin #15 (D4) */
+    u8_val = 0;
+    /*prepare for OR*/
+    for (u8_row = 32; u8_row < 40; u8_row++) {
+      u8_val |= u8_andmap[u8_row];
+    }
+    u8_columnvalue[18] = (char) ((1 - u8_val) & 0xFF);
+    u8_columnvalue[19] = (char) ((u8_val) & 0xFF);
+    u8_outvalue[4] = (char) ((1 - u8_val) & 0xFF);
+
+    /* O pin #14 (D5) */
+    u8_val = 0;
+    /*prepare for OR*/
+    for (u8_row = 40; u8_row < 48; u8_row++) {
+      u8_val |= u8_andmap[u8_row];
+    }
+    u8_columnvalue[22] = (char) ((1 - u8_val) & 0xFF);
+    u8_columnvalue[23] = (char) ((u8_val) & 0xFF);
+    u8_outvalue[5] = (char) ((1 - u8_val) & 0xFF);
+
+    /* O pin #13 (D6) */
+    u8_val = 0;
+    /*prepare for OR*/
+    for (u8_row = 48; u8_row < 56; u8_row++) {
+      u8_val |= u8_andmap[u8_row];
+    }
+    u8_columnvalue[26] = (char) ((1 - u8_val) & 0xFF);
+    u8_columnvalue[27] = (char) ((u8_val) & 0xFF);
+    u8_outvalue[6] = (char) ((1 - u8_val) & 0xFF);
+
+    /* I/O pin #12 */
+    u8_val = 0;
+    /*prepare for OR*/
+    for (u8_row = 57; u8_row < 64; u8_row++) {
+      u8_val |= u8_andmap[u8_row];
+    }
+    if (u8_andmap[56] == 1) {
+      u8_columnvalue[30] = (char) ((1 - u8_val) & 0xFF);
+      u8_columnvalue[31] = (char) ((u8_val) & 0xFF);
+      u8_outvalue[7] = (char) ((1 - u8_val) & 0xFF);
+    } else {
+      /*pin is in INPUT configuration so it doesn't create output...*/
+      u8_columnvalue[30] = 0;
+      u8_columnvalue[31] = 1;
+    }
+  }
+
+  public static WriteHandlerPtr bagman_pal16r6_w =
+      new WriteHandlerPtr() {
+        public void handler(int offset, int data) {
+          /*unsigned*/
+          char u8_line;
+
+          u8_line = (char) (((offset /*+ 1*/) << 2) & 0xFF);
+          u8_columnvalue[u8_line] = (char) ((1 - (data & 1)) & 0xFF);
+          u8_columnvalue[u8_line + 1] = (char) ((data & 1) & 0xFF);
+        }
+      };
+
+  public static InitMachinePtr bagman_machine_init =
+      new InitMachinePtr() {
+        public void handler() {
+          bagman_pal16r6_w.handler(0, 1);
+          bagman_pal16r6_w.handler(1, 1);
+          bagman_pal16r6_w.handler(2, 1);
+          bagman_pal16r6_w.handler(3, 1);
+          bagman_pal16r6_w.handler(4, 1);
+          bagman_pal16r6_w.handler(5, 1);
+          bagman_pal16r6_w.handler(6, 1);
+          bagman_pal16r6_w.handler(7, 1);
+          update_pal();
+        }
+      };
+
+  public static ReadHandlerPtr bagman_pal16r6_r =
+      new ReadHandlerPtr() {
+        public int handler(int offset) {
+          update_pal();
+          return (u8_outvalue[6])
+              + (u8_outvalue[5] << 1)
+              + (u8_outvalue[4] << 2)
+              + (u8_outvalue[3] << 3)
+              + (u8_outvalue[2] << 4)
+              + (u8_outvalue[1] << 5);
+
+          /* Bagman schematics show that this is right mapping order of PAL outputs to bits.
+           ** This is the PAL 16R6 shown almost in the middle of the schematics.
+           ** The /RD4 line goes low (active) whenever CPU reads from memory address a000.
+           */
+        }
+      };
+}
