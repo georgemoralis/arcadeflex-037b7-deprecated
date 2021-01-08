fa15b4fe7d44275d1c75468034c8af9b98ab564c
georgemoralis <giorgosmrls@gmail.com>
2021-01-08 17:51:46 +0200

    Added exctsccr,exedexes,fastfred

--- emulator_complete/src/main/java/gr/codebb/arcadeflex/v037b7/vidhrdw/jrpacman.java
+++ emulator_complete/src/main/java/gr/codebb/arcadeflex/v037b7/vidhrdw/jrpacman.java
@@ -4,8 +4,8 @@
  */
 package gr.codebb.arcadeflex.v037b7.vidhrdw;
 
-import static gr.codebb.arcadeflex.common.libc.cstring.*;
 import static gr.codebb.arcadeflex.common.PtrLib.*;
+import static gr.codebb.arcadeflex.common.libc.cstring.*;
 import static gr.codebb.arcadeflex.mame.osdependH.*;
 import static gr.codebb.arcadeflex.old.mame.drawgfx.*;
 import static gr.codebb.arcadeflex.old2.mame.common.*;
