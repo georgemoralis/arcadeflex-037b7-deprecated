e660af4baab18e1d1df3d8e5f71a029c723c7102
georgemoralis <giorgosmrls@gmail.com>
2021-01-14 23:32:38 +0200

    Added working gsword,speebal,timeplt,pkunwar driver

--- emulator_complete/src/main/java/gr/codebb/arcadeflex/v037b7/drivers/pooyan.java
+++ emulator_complete/src/main/java/gr/codebb/arcadeflex/v037b7/drivers/pooyan.java
@@ -10,7 +10,7 @@ import static gr.codebb.arcadeflex.WIP.v037b7.mame.memoryH.*;
 import static gr.codebb.arcadeflex.WIP.v037b7.mame.sndintrf.*;
 import static gr.codebb.arcadeflex.mame.driverH.*;
 import static gr.codebb.arcadeflex.old.mame.cpuintrf.*;
-import static gr.codebb.arcadeflex.sndhrdw.timeplt.*;
+import static gr.codebb.arcadeflex.v037b7.sndhrdw.timeplt.*;
 import static gr.codebb.arcadeflex.v037b7.common.fucPtr.*;
 import static gr.codebb.arcadeflex.re.v037b7.mame.commonH.*;
 import static gr.codebb.arcadeflex.re.v037b7.mame.drawgfxH.*;
